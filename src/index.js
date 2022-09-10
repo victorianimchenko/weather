cbfd82f9844774a1b463f0cb895d63111141dac6
victorianimchenko <107873627+victorianimchenko@users.noreply.github.com>
2022-09-10 23:13:12 +0300

    cards is complited and added background

--- src/index.js
+++ src/index.js
@@ -36,8 +36,6 @@ let dataTemp = null;
 navigator.geolocation.getCurrentPosition(handlePosition);
 
 
-
-
 function showTemp(response) {
   let temperature = Math.round(response.data.main.temp);
   dataTemp = temperature;
@@ -68,9 +66,87 @@ function showTemp(response) {
   descriptionEl.innerHTML = `${response.data.weather[0].description}`;
   precipitationEl.innerHTML = `${response.data.main.humidity}%`;
   windEl.innerHTML = `${response.data.wind.speed}km/h`;
+
+
+  getForecast(response.data.coord)
   
   
 }
+
+
+function displayForecast(response) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = ` <div class="row row-cols-1 row-cols-md-5 g-4">`;
+  let indexCurrentDay = currentDate.getDay();
+  forecast.forEach(function (forecastDay, index) {
+    
+        if (index < 5) {
+    let currentDay = indexCurrentDay + index; 
+        
+    if(currentDay > 5)
+    {
+       indexCurrentDay = -1;
+    }
+  
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col">
+            <div class="card h-100">
+             
+              <div class="card-body center">
+                <h5 class="card-title temperature">${days[currentDay]}</h5>
+              <img src=${getAddressImage(forecastDay.weather[0].main, forecastDay.weather[0].description)} />
+
+              <div class="card-text temperature">
+                <span class="forecast-temperature-max">
+                ${Math.round(
+                  response.data.daily[index].temp.max
+                )}°
+                </span>
+                <span class="forecast-temperature-min">
+                ${Math.round(
+                  response.data.daily[index].temp.min
+                )}°
+                </span>
+              </div>
+              </div>
+            </div>
+          </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+function getAddressImage(weather, description){
+
+if (weather === "Clear") {
+  return "images/sunny.png";
+}
+if (weather === "Clouds") {
+  return "images/cloudy.png";
+}
+if (weather === "Rain") {
+  if (
+    description === "light intensity shower rain"
+  ) {
+    return "images/rain_light.png";
+  } else {
+    return "images/rain.png";
+  }
+}
+if (weather === "Thunderstorm") {
+  return "images/thunderstorms.png";
+}
+}
+
+
 
 async function searchCity(event) {
   event.preventDefault();
@@ -96,7 +172,6 @@ function switchTemperature(event) {
 }
 
 async function handlePosition(position) {
-  console.log(position)
 
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -110,9 +185,21 @@ async function showCurrentPosition(event) {
   await navigator.geolocation.getCurrentPosition(handlePosition);
 }
 
+
+
+ async function getForecast(coordinates){
+  let apiKeyFor = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKeyFor}&units=metric`;
+
+  await axios.get(apiUrl).then(displayForecast);
+}
+
+
+
+
 formEl.addEventListener("submit", searchCity);
 search.addEventListener("click", searchCity);
 temperatureEl.addEventListener("click", switchTemperature);
 faringateEl.addEventListener("click", switchTemperature);
 btn.addEventListener("click", showCurrentPosition);
-// imageEl.addEventListener()
+
